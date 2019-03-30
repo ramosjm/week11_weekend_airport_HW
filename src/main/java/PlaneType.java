6f3383543dc34252a748ea977893fe2e9f1362ff
ramosjm <jjramos_2004@hotmail.com>
2019-03-30 21:10:03 +0000

    added capacity to plane enum.

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,10 +1,20 @@
 public enum PlaneType {
-    BOING747,
-    BOING767,
-    BOING777,
-    BOING787,
-    AIRBUSA300,
-    AIRBUSA310,
-    AIRBUSA330,
+    BOING747(366),
+    BOING767(181),
+    BOING777(451),
+    BOING787(290),
+    AIRBUSA300(266),
+    AIRBUSA310(220),
+    AIRBUSA330(335);
+
+    private final int capacity;
+
+    PlaneType(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
 
 }
